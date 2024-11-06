6866b785cae2abc3840e9a8d125e3e399f636ecb
jgvlf <joaogabrielvlf@gmail.com>
2024-11-06 11:20:38 -0300

    Adding the Async GET Test

---
+++ test/http/asyncreq/test_get_async.py
@@ -0,0 +1,25 @@
+import os
+
+from allquests.http.asyncreq.get import get
+from allquests.http.methods import Methods
+from allquests.http.proxy import Proxy
+from allquests.http.request_args import RequestArgs
+
+proxy: Proxy | None = None
+
+if os.environ.get("http_proxy") or os.environ.get("https_proxy"):
+    proxy = Proxy("localhost", 3128)
+
+params: RequestArgs = RequestArgs(
+    Methods.GET,
+    "/api/v2/pokemon/ditto",
+    host="pokeapi.co",
+    proxy=proxy if proxy else None,
+)
+
+
+def test_get_async_request():
+    res = get(params)
+    if res.status != 200:
+        msg = f"The request don't complete succesfull. Response get:{res.status}."
+        raise RuntimeError(msg)
